440cdf4fe23eb9cdf2a97505600e8416d32a15ad
Misal <43807190+codemissile@users.noreply.github.com>
2024-05-17 15:31:14 +0100

    project file

---
+++ Profile.xaml.cs
@@ -0,0 +1,18 @@
+using MauiApp2;
+using Microsoft.Maui.Controls;
+using System;
+
+namespace MauiApp2
+{
+    public partial class Profile : ContentPage
+    {
+
+        public Profile()
+        {
+            InitializeComponent();
+
+        }
+
+
+    }
+}
---
+++ Sensors.xaml.cs
@@ -0,0 +1,15 @@
+using Microsoft.Maui.Devices.Sensors;
+
+namespace MauiApp2;
+
+public partial class Sensors : ContentPage
+{
+    
+    public Sensors()
+    {
+        InitializeComponent();
+    }
+
+
+
+}
